3b33c2ab5db588c78cb4fa2de29e348fc9a9238f
elhani <52415112+elhani@users.noreply.github.com>
2019-08-19 14:43:04 +0200

    Update sami1.js

--- sami1.js
+++ sami1.js
@@ -10,43 +10,7 @@
   } 
 console.log(echec)
 
-function chesscheval(cell) {
-    var possibleCoordinates = [];
-  var cellX=0;
-  var cellY=0;
-    
-    
-    //Find all possible X Positions
-    var cellXpositions = [cellX + 2, cellX - 2, cellX + 1, cellX - 1].filter(function(cellPosition) {
-        return (cellPosition > 0 && cellPosition < M);
-    })
-    
-    //Find all possible Y Positions
-    var cellYpositions = [cellY + 2, cellY - 2, cellY + 1, cellY - 1].filter(function(cellPosition) {
-        return (cellPosition > 0 && cellPosition < M);
-    })
-    
-    
 
-    for (var i = 0; i < cellXpositions.length; i++) {
-        for (var j = 0; j < cellYpositions.length; j++) {
-            if (Math.abs(cellX - cellXpositions[i]) + Math.abs(cellY - cellYpositions[j]) === 3) {
-                console.log('This is a valid coordinate: ', [cellXpositions[i], cellYpositions[j]]);
-                if (!possibleCoordinates.includes([cellXpositions[i], cellYpositions[j]])) {
-                    possibleCoordinates.push([cellXpositions[i], cellYpositions[j]]);
-                   
-                } 
-            }
-        }
-    }
-    console.log('Possible Coordinates:', possibleCoordinates);
-    console.log ('Possible Moves:', possibleCoordinates.length);
-    return possibleCoordinates.length;
-
-
-    
-}
-chesscheval();
 
 
 var findMinSteps = function(depart, pointA) {
